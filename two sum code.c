9bae33bba4ed5d318d91fade7c839455a297e897
niloyraytridib <niloyraytridip849@gmail.com>
2025-07-11 17:21:59 +0600

    Add files via upload

---
+++ two sum code.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+  int a, b, sum;
+  printf("Enter the integer value: ");
+  scanf("%d%d", &a, &b);
+  sum= a+b;
+  printf("The summation of three number is:%d", sum);
+  return 0;
+}

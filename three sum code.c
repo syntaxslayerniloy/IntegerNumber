c1122b19cb741f190af89784906ea71f860c1029
niloyraytridib <niloyraytridip849@gmail.com>
2025-07-11 17:24:12 +0600

    Add files via upload

---
+++ three sum code.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int a, b, c, sum;
+printf("Enter the integer value: ");
+scanf("%d%d%d", &a, &b, &c);
+sum= a+b+c;
+printf("The summation of three number is:%d", sum);
+return 0;
+}

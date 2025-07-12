779e6e5732c44a7919b8d8972a177eaf0e2b9cb8
niloyraytridib <niloyraytridip849@gmail.com>
2025-07-12 20:43:21 +0600

    Add files via upload

---
+++ showing pass or fail code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num1;
+printf("Enter your number:");
+scanf("%d",&num1);
+if(num1>=33)
+    printf("\n you pass in the examination");
+else if(num1<33)
+    printf("\n you fail in the examination");
+return 0;
+
+}
+
